2acc3a160160cd093b18949ad7bb17b638412878
jose.molina <jose.molina@nexcel.cl>
2022-04-28 14:34:50 -0400

    test for github action

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const { sequelize } = require("./database")
 async function main() {
   try {
     await app.listen(PORT)
-    console.log(`Server is running on port : ${PORT}`)
+    console.log(`Server is running on port: ${PORT}`)
     await sequelize.sync()
   } catch (error) {
     console.error("Some error: ", error)
